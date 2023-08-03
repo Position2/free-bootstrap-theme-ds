b20c52045c0b4ad5cd8d63844c65e7958a7e54f0
Megha BM <megha.bm@gop2.com>
2023-08-03 16:11:17 +0530

    updated bootstrap and jquery bxslider fancybox

--- script/plugin-active.js
+++ script/plugin-active.js
@@ -1,46 +1,47 @@
 $(document).ready(function() {
   // Mobile menu trigger script
-  $(".menu-trigger").click(function() {
-      $(this).toggleClass('active');
-      $(".mobile-menu").toggleClass('visible');
-  });
+  $(document).on("click", ".menu-trigger", function() {
+    $(this).toggleClass('active');
+    $(".mobile-menu").toggleClass('visible');
+});
   // Smoothscroll script
-  $('.nav-link').click(function() {
-      var dis = $(this),
-          disTarget = dis.data('target'),
-          ScrollTo = $(disTarget).offset().top;
-      dis.addClass('active').siblings('.nav-link').removeClass('active');
-      $('html,body').animate({ scrollTop: ScrollTo });
-  });
+  $(document).on("click", ".nav-link", function() {
+    var dis = $(this),
+        disTarget = dis.data('target'),
+        ScrollTo = $(disTarget).offset().top;
+
+    dis.addClass('active').siblings('.nav-link').removeClass('active');
+    $('html, body').animate({ scrollTop: ScrollTo });
+});
   // contact form script
-  $('.form-wrap input').blur(function() {
-      tmpval = $(this).val();
-      if (tmpval == '') {
-          $(this).addClass('empty');
-          $(this).removeClass('not-empty');
-      } else {
-          $(this).addClass('not-empty');
-          $(this).removeClass('empty');
-      }
-  });
+  $(document).on("blur", ".form-wrap input", function() {
+    var tmpval = $(this).val();
+    if (tmpval == '') {
+        $(this).addClass('empty').removeClass('not-empty');
+    } else {
+        $(this).addClass('not-empty').removeClass('empty');
+    }
+});
   // testimonial slider
-  $('.testimonial-slider').bxSlider({
-      auto: true,
-      mode: 'fade',
-      infiniteLoop: true,
-      controls: false
-  });
+  $(document).on("init", ".testimonial-slider", function(){
+    // Code inside this block will run after the slider is initialized
+}).bxSlider({
+    auto: true,
+    mode: 'fade',
+    infiniteLoop: true,
+    controls: false
+});
   // Changing the defaults 
   window.sr = ScrollReveal();
   // Customizing a reveal set 
   sr.reveal('.each-service', { origin: 'bottom', distance: '100px', duration: 1000, delay: 0, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)' });
   // sript for fixed header on scroll
-  $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 60) {
-          $("#Header").addClass("header-fixed");
-      } else {
-          $("#Header").removeClass("header-fixed");
-      }
-  });
+  $(document).on("scroll", function() {
+    var scroll = $(this).scrollTop();
+    if (scroll >= 60) {
+        $("#Header").addClass("header-fixed");
+    } else {
+        $("#Header").removeClass("header-fixed");
+    }
+});
 });
